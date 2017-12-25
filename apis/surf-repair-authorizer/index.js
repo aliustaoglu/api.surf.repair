466cbf0e79ffce3c05236d4f09c9981ee2478484
aliustaoglu <aliustaoglu@yahoo.com>
2017-12-26 11:45:45 +1300

    JWT verification in Authorizer

--- apis/surf-repair-authorizer/index.js
+++ apis/surf-repair-authorizer/index.js
@@ -1,44 +1,37 @@
 'use strict';
+const signingKey = 'cuneyt'; // TODO: Change in AWS Console after deployed
 
-exports.handler =  function(event, context, callback) {
+exports.handler = function(event, context, callback) {
   var token = event.authorizationToken;
-  switch (token.toLowerCase()) {
-      case 'allow':
-          callback(null, generatePolicy('user', 'Allow', event.methodArn));
-          break;
-      case 'deny':
-          callback(null, generatePolicy('user', 'Deny', event.methodArn));
-          break;
-      case 'unauthorized':
-          callback("Unauthorized");   // Return a 401 Unauthorized response
-          break;
-      default:
-          callback("Error: Invalid token"); 
-  }
+  const nJwt = require('./njwt');
+  nJwt.verify(token, signingKey, function(err, verifiedJwt) {
+    if (err) {
+      callback('Unauthorized');
+    } else {
+      console.log(verifiedJwt);
+      callback(null, generatePolicy('user', 'Allow', event.methodArn, verifiedJwt.body));
+    }
+  });
 };
 
 // Help function to generate an IAM policy
-var generatePolicy = function(principalId, effect, resource) {
+var generatePolicy = function(principalId, effect, resource, verifiedJwt) {
   var authResponse = {};
-  
+
   authResponse.principalId = principalId;
   if (effect && resource) {
-      var policyDocument = {};
-      policyDocument.Version = '2012-10-17'; 
-      policyDocument.Statement = [];
-      var statementOne = {};
-      statementOne.Action = 'execute-api:Invoke'; 
-      statementOne.Effect = effect;
-      statementOne.Resource = resource;
-      policyDocument.Statement[0] = statementOne;
-      authResponse.policyDocument = policyDocument;
+    var policyDocument = {};
+    policyDocument.Version = '2012-10-17';
+    policyDocument.Statement = [];
+    var statementOne = {};
+    statementOne.Action = 'execute-api:Invoke';
+    statementOne.Effect = effect;
+    statementOne.Resource = resource;
+    policyDocument.Statement[0] = statementOne;
+    authResponse.policyDocument = policyDocument;
   }
-  
+
   // Optional output with custom properties of the String, Number or Boolean type.
-  authResponse.context = {
-      "stringKey": "stringval",
-      "numberKey": 123,
-      "booleanKey": true
-  };
+  authResponse.context = verifiedJwt;
   return authResponse;
-}
+};
--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const context = {
 
 };
 const event = {
-  key3: 'value3',
+  authorizationToken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJTdXJmIFJlcGFpciBBUEkiLCJpYXQiOjE1MTQyMzIzOTAsImV4cCI6MTU0NTc2ODM5MCwiYXVkIjoic3VyZi5yZXBhaXIiLCJzdWIiOiJhZG1pbkBzdXJmIiwiUm9sZSI6ImZpeGVyIiwiaWQiOiIzNzkwYTk4NC0wZmZmLTQyZjctODI1OC00MDI3YjlhZjI3ODUiLCJhbGlhcyI6ImZpeGVyMSJ9.hXNEbu_qLZzVLbkX5M019OGHogXGqMYij4_wtFUZV-M',
   key2: 'value2',
   key1: 'value1'
 };
@@ -36,6 +36,6 @@ const callback = (error, result) => {
 
 // Update below as your needs for debugging
 // The function you like to debug etc
-const helloApi = require('./apis/helloApi/index');
+const myApi = require('./apis/surf-repair-authorizer/index');
 var fs = require('fs');
-const m = mapData.handler(event, context, callback);
+const m = myApi.handler(event, context, callback);
